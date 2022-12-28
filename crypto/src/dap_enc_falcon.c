6c9ccfc6e1f7203a7a618061b845e0e69eae3c0d
Eveniy Kramsakov <sobiegrus@gmail.com>
2022-12-28 20:10:40 +0300

    [+] Add some bindings

--- crypto/src/dap_enc_falcon.c
+++ crypto/src/dap_enc_falcon.c
@@ -1,6 +1,7 @@
 
 #include "dilithium_params.h"
 #include "dap_enc_falcon.h"
+#include "falcon.h"
 
 #define LOG_TAG "dap_enc_sig_falcon"
 
@@ -19,43 +20,43 @@ void dap_enc_sig_falcon_key_new(struct dap_enc_key *key) {
 }
 
 void dap_enc_sig_falcon_key_new_generate(struct dap_enc_key *key, const void *kex_buf, size_t kex_size,
-        const void* seed, size_t seed_size, size_t key_size)
-{
-    (void) kex_buf;
-    (void) kex_size;
-    (void) seed;
-    (void) seed_size;
-    (void) key_size;
+        const void* seed, size_t seed_size, size_t key_size) {
+
 
     key->type = DAP_ENC_KEY_TYPE_SIG_FALCON;
     key->enc = NULL;
     key->enc_na = (dap_enc_callback_dataop_na_t) dap_enc_sig_falcon_get_sign;
     key->dec_na = (dap_enc_callback_dataop_na_t) dap_enc_sig_falcon_verify_sign;
 
-    falcon_private_key_t *private_key = NULL;
-    falcon_public_key_t *public_key = NULL;
 
+    int retcode = 0;
+    unsigned int logn = 0;
     if (s_falcon_sign_degree == FALCON_512) {
-        private_key = falcon512_privkey_new();
-        public_key = falcon512_pubkey_new();
+        logn = 9;
     } else if (s_falcon_sign_degree == FALCON_1024) {
-        private_key = falcon1024_privkey_new();
-        public_key = falcon1024_pubkey_new();
+        logn = 10;
     } else {
         log_it(L_ERROR, "Unknown falcon sign degree");
         return;
     }
+    size_t tmp[FALCON_TMPSIZE_KEYGEN(logn)];
 
-    if (private_key == NULL || public_key == NULL) {
-        log_it(L_ERROR, "Can't allocate memory for falcon key");
+
+    shake256_context* rng = NULL;
+    retcode = shake256_init_prng_from_system(rng);
+    if (retcode != 0) {
+        log_it(L_ERROR, "Failed to initialize PRNG");
         return;
     }
 
-    if (falcon_keygen(private_key, public_key) != 0) {
-        log_it(L_ERROR, "Can't generate falcon key");
+    retcode = falcon_keygen_make(
+            rng,
+            logn,
+            &key->priv_key_data, key->priv_key_data_size,
+            &key->pub_key_data, key->pub_key_data_size,
+            tmp, FALCON_TMPSIZE_KEYGEN(logn));
+    if (retcode != 0) {
+        log_it(L_ERROR, "Failed to generate falcon key");
         return;
     }
-
-    key->data = private_key;
-    key->pub_key_data = public_key;
 }
