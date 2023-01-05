e92589b40f1b272646052226efe44698856f6dbb
Eveniy Kramsakov <sobiegrus@gmail.com>
2023-01-05 16:10:37 +0300

    [+] Add signature serialization/deserialization

--- crypto/src/dap_enc_falcon.c
+++ crypto/src/dap_enc_falcon.c
@@ -344,6 +344,103 @@ falcon_public_key_t* dap_enc_falcon_read_public_key(const uint8_t* a_buf, size_t
     return l_public_key;
 }
 
+uint8_t* dap_enc_sig_falcon_write_signature(falcon_signature_t* a_sign, size_t *a_sign_out) {
+
+    if (!a_sign) {
+        log_it(L_ERROR, "::write_signature() a_sign is NULL");
+        return NULL;
+    }
+
+
+    size_t l_buflen = sizeof(uint64_t) + sizeof(s_falcon_sign_degree) + sizeof(s_falcon_kind) + sizeof(s_falcon_type) + a_sign->sig_len + sizeof(uint64_t);
+
+    uint8_t* l_buf = DAP_NEW_Z_SIZE(uint8_t, l_buflen);
+    if (!l_buf) {
+        log_it(L_ERROR, "::write_signature() l_buf is NULL — memory allocation error");
+        return NULL;
+    }
+
+    memcpy(l_buf, &l_buflen, sizeof(uint64_t));
+    memcpy(l_buf + sizeof(uint64_t), &a_sign->degree, sizeof(s_falcon_sign_degree));
+    memcpy(l_buf + sizeof(uint64_t) + sizeof(s_falcon_sign_degree), &a_sign->kind, sizeof(s_falcon_kind));
+    memcpy(l_buf + sizeof(uint64_t) + sizeof(s_falcon_sign_degree) + sizeof(s_falcon_kind), &a_sign->type, sizeof(s_falcon_type));
+    memcpy(l_buf + sizeof(uint64_t) + sizeof(s_falcon_sign_degree) + sizeof(s_falcon_kind) + sizeof(s_falcon_type), &a_sign->sig_len, sizeof(uint64_t));
+    memcpy(l_buf + sizeof(uint64_t) + sizeof(s_falcon_sign_degree) + sizeof(s_falcon_kind) + sizeof(s_falcon_type) + sizeof(uint64_t), a_sign->sig_data, a_sign->sig_len);
+
+    if (a_sign_out) {
+        *a_sign_out = l_buflen;
+    }
+
+    return l_buf;
+
+}
+falcon_signature_t* dap_enc_falcon_read_signature(uint8_t* a_buf, size_t a_buflen) {
+    if (!a_buf) {
+        log_it(L_ERROR, "::read_signature() a_buf is NULL");
+        return NULL;
+    }
+
+    if (a_buflen < (sizeof(uint64_t) + sizeof(s_falcon_sign_degree) + sizeof(s_falcon_kind) + sizeof(s_falcon_type) + sizeof(uint64_t))) {
+        log_it(L_ERROR, "::read_signature() a_buflen %zd is smaller than first five fields(%zd)", a_buflen, (sizeof(uint64_t) + sizeof(s_falcon_sign_degree) + sizeof(s_falcon_kind) + sizeof(s_falcon_type) + sizeof(uint64_t)));
+        return NULL;
+    }
+
+    uint64_t l_buflen = 0;
+    memcpy(&l_buflen, a_buf, sizeof(uint64_t));
+
+    if (l_buflen != a_buflen) {
+        log_it(L_ERROR, "::read_signature() l_buflen %zd is not equal to a_buflen %zd", l_buflen, a_buflen);
+        return NULL;
+    }
+
+    falcon_sign_degree_t l_degree = 0;
+    falcon_kind_t l_kind = 0;
+    falcon_sign_type_t l_type = 0;
+    size_t l_sig_len = 0;
+
+    memcpy(&l_degree, a_buf + sizeof(uint64_t), sizeof(s_falcon_sign_degree));
+
+    if (l_degree != FALCON_512 && l_degree != FALCON_1024) { // we are now supporting only 512 and 1024 degrees
+        log_it(L_ERROR, "::read_signature() l_degree %d is not supported", l_degree);
+        return NULL;
+    }
+
+    memcpy(&l_kind, a_buf + sizeof(uint64_t) + sizeof(s_falcon_sign_degree), sizeof(s_falcon_kind));
+
+    if (l_kind != FALCON_COMPRESSED && l_kind != FALCON_PADDED && l_kind != FALCON_CT) { // we are now supporting only compressed, padded and ct signatures
+        log_it(L_ERROR, "::read_signature() l_kind %d is not supported", l_kind);
+        return NULL;
+    }
+
+    memcpy(&l_type, a_buf + sizeof(uint64_t) + sizeof(s_falcon_sign_degree) + sizeof(s_falcon_kind), sizeof(s_falcon_type));
+
+    if (l_type != FALCON_DYNAMIC && l_type != FALCON_TREE) { // we are now supporting only sign and sign open signatures
+        log_it(L_ERROR, "::read_signature() l_type %d is not supported", l_type);
+        return NULL;
+    }
+
+    memcpy(&l_sig_len, a_buf + sizeof(uint64_t) + sizeof(s_falcon_sign_degree) + sizeof(s_falcon_kind) + sizeof(s_falcon_type), sizeof(uint64_t));
+
+    //todo: do we need to check if signature length is correct?
+
+    if (l_buflen < (sizeof(uint64_t) + sizeof(s_falcon_sign_degree) + sizeof(s_falcon_kind) + sizeof(s_falcon_type) + sizeof(uint64_t) + l_sig_len )) {
+        log_it(L_ERROR, "::read_signature() l_buflen %zd is smaller than fields(%zd)", l_buflen, (sizeof(uint64_t) + sizeof(s_falcon_sign_degree) + sizeof(s_falcon_kind) + sizeof(s_falcon_type) + sizeof(uint64_t) + l_sig_len));
+        return NULL;
+    }
+
+    falcon_signature_t *l_sign = DAP_NEW(falcon_signature_t);
+
+    l_sign->degree = l_degree;
+    l_sign->kind = l_kind;
+    l_sign->type = l_type;
+    l_sign->sig_len = l_sig_len;
+    l_sign->sig_data = DAP_NEW_SIZE(uint8_t, l_sig_len);
+
+    memcpy(l_sign->sig_data, a_buf + sizeof(uint64_t) + sizeof(s_falcon_sign_degree) + sizeof(s_falcon_kind) + sizeof(s_falcon_type) + sizeof(uint64_t), l_sig_len);
+
+    return l_sign;
+}
+
 
 void falcon_private_and_public_keys_delete(falcon_private_key_t* privateKey, falcon_public_key_t* publicKey) {
     falcon_private_key_delete(privateKey);
--- crypto/src/falcon/falcon_params.h
+++ crypto/src/falcon/falcon_params.h
@@ -47,25 +47,14 @@ typedef struct {
     falcon_kind_t kind;
     falcon_sign_degree_t degree;
     falcon_sign_type_t type;
-    unsigned char* sig_data;
     uint64_t sig_len;
+    unsigned char* sig_data;
 } falcon_signature_t;
 
-
-
-bool falcon_params_init(falcon_param_t *, falcon_kind_t);
-
-int falcon_crypto_sign_keypair(falcon_public_key_t* public_key, falcon_private_key_t* private_key,
-                               falcon_kind_t kind, const void* seed, size_t seed_size);
-
-int falcon_crypto_sign(falcon_signature_t*, const unsigned char*, unsigned long long, const falcon_private_key_t*);
-
-int falcon_crypto_sign_open(unsigned char*, unsigned long long, falcon_signature_t*, const falcon_public_key_t*);
 
 void falcon_private_key_delete(falcon_private_key_t* private_key);
 void falcon_public_key_delete(falcon_public_key_t* public_key);
 void falcon_private_and_public_keys_delete(falcon_private_key_t* private_key, falcon_public_key_t* public_key);
 
-void falcon_signature_delete(falcon_signature_t* sig);
 
 #endif //CELLFRAME_SDK_FALCON_PARAMS_H
