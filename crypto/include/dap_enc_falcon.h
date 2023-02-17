e84e95ee3a8e915922a4f9ab35ef4591edbde1f5
Roman Khlopkov <roman.khlopkov@demlabs.net>
2023-02-17 14:06:11 +0300

    [*] Falcon repair

--- crypto/include/dap_enc_falcon.h
+++ crypto/include/dap_enc_falcon.h
@@ -5,9 +5,9 @@
 #include "falcon/falcon_params.h"
 
 
-void dap_enc_sig_falcon_set_degree(enum DAP_FALCON_SIGN_DEGREE a_falcon_sign_degree);
+void dap_enc_sig_falcon_set_degree(falcon_sign_degree_t a_falcon_sign_degree);
 void dap_enc_sig_falcon_set_kind(falcon_kind_t a_falcon_kind);
-void dap_enc_sig_falcon_set_type(enum DAP_FALCON_SIGN_TYPE a_falcon_sign_type);
+void dap_enc_sig_falcon_set_type(falcon_sign_type_t a_falcon_sign_type);
 
 void dap_enc_sig_falcon_key_new(struct dap_enc_key *key);
 
