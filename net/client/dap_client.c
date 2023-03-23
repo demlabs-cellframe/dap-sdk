9b56db608abf05f51003c144071db4f13bca4e2c
daniil.frolov <daniil.frolov@demlabs.net>
2023-03-23 14:22:37 +0700

    roll back some previous changes

--- net/client/dap_client.c
+++ net/client/dap_client.c
@@ -581,20 +581,6 @@ dap_stream_ch_t * dap_client_get_stream_ch_unsafe(dap_client_t * a_client, uint8
     return l_ch;
 }
 
-dap_stream_ch_t ** dap_client_get_pointer_stream_ch_unsafe(dap_client_t * a_client, uint8_t a_ch_id)
-{
-    dap_stream_ch_t ** l_ch = NULL;
-    dap_client_pvt_t * l_client_internal = a_client ? DAP_CLIENT_PVT(a_client) : NULL;
-    if(l_client_internal && l_client_internal->stream && l_client_internal->stream_es)
-        for(size_t i = 0; i < l_client_internal->stream->channel_count; i++) {
-            if(l_client_internal->stream->channel[i]->proc->id == a_ch_id) {
-                l_ch = &l_client_internal->stream->channel[i];
-                break;
-            }
-        }
-    return l_ch;
-}
-
 /**
  * @brief dap_client_get_stream_id
  * @param a_client
