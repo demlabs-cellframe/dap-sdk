4912a88f244bc2475337aeebffa9ba43a58af3c4
Constantin <papizh.konstantin@demlabs.net>
2023-07-04 16:42:08 +0700

    Linkage issues fixed

--- crypto/include/dap_enc_kyber.h
+++ crypto/include/dap_enc_kyber.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "dap_enc_key.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "kyber512.h"
+#include "kem.h"
+
 void   dap_enc_kyber512_key_new_from_data_public( dap_enc_key_t * a_key, const void * a_in, size_t a_in_size );
 void   dap_enc_kyber512_key_new( dap_enc_key_t* a_key );
 size_t dap_enc_kyber512_gen_bob_shared_key ( dap_enc_key_t *b_key, const void *a_pub, size_t a_pub_size, void ** b_pub );
@@ -8,3 +15,6 @@ size_t dap_enc_kyber512_gen_alice_shared_key ( dap_enc_key_t *a_key, const void 
 void   dap_enc_kyber512_key_delete( dap_enc_key_t* a_key );
 void   dap_enc_kyber512_key_generate( dap_enc_key_t * a_key, const void *kex_buf, size_t kex_size, const void * seed, size_t seed_size, size_t key_size );
 
+#ifdef __cplusplus
+}
+#endif
