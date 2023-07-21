dae88b58f8504cda1b97847e820053a6c5208929
Constantin P <papizh.konstantin@demlabs.net>
2023-07-21 18:58:13 +0700

    ...

--- io/dap_events_socket.c
+++ io/dap_events_socket.c
@@ -1043,9 +1043,11 @@ static void s_add_ptr_to_buf(dap_events_socket_t * a_es, void* a_arg)
     if (a_es->buf_out_size_max < a_es->buf_out_size + sizeof(void*)) {
         a_es->buf_out_size_max += l_basic_buf_size;
         a_es->buf_out = DAP_REALLOC(a_es->buf_out, a_es->buf_out_size_max);
+        log_it(L_MSG, "[!] Increase capacity to %lu, actual size: %lu", a_es->buf_out_size_max, a_es->buf_out_size);
     } else if ((a_es->buf_out_size + sizeof(void*) <= l_basic_buf_size / 2) && (a_es->buf_out_size_max > l_basic_buf_size)) {
         a_es->buf_out_size_max = l_basic_buf_size;
         a_es->buf_out = DAP_REALLOC(a_es->buf_out, a_es->buf_out_size_max);
+        log_it(L_MSG, "[!] Decrease capacity to %lu, actual size: %lu", a_es->buf_out_size_max, a_es->buf_out_size);
     }
     *(void**)(a_es->buf_out + a_es->buf_out_size) = a_arg;
     a_es->buf_out_size += sizeof(a_arg);
