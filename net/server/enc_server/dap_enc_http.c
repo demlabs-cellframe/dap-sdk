f555f1f146b8659f3f7c8354bd1fc86c00ccf08b
alexey.stratulat <alexey.stratulat@demlabs.net>
2023-08-22 07:53:23 +0000

    Feature 8973

--- net/server/enc_server/dap_enc_http.c
+++ net/server/enc_server/dap_enc_http.c
@@ -47,7 +47,9 @@
 #include "dap_enc_base64.h"
 #include "dap_enc_msrln.h"
 #include "include/http_status_code.h"
+#include "dap_enc_http_ban_list_client.h"
 #include "json.h"
+#include "dap_enc_http_ban_list_client.h"
 
 
 #define LOG_TAG "dap_enc_http"
@@ -56,6 +58,7 @@ static dap_enc_acl_callback_t s_acl_callback = NULL;
 
 int enc_http_init()
 {
+    dap_enc_http_ban_list_client_init();
     return 0;
 }
 
@@ -94,6 +97,11 @@ void enc_http_proc(struct dap_http_simple *cl_st, void * arg)
 {
     log_it(L_DEBUG,"Proc enc http request");
     http_status_code_t * return_code = (http_status_code_t*)arg;
+
+    if (dap_enc_http_ban_list_client_check_ipv4(cl_st->http_client->esocket->remote_addr.sin_addr)) {
+        *return_code = Http_Status_Forbidden;
+        return;
+    }
 
     if(strcmp(cl_st->http_client->url_path,"gd4y5yh78w42aaagh") == 0 ) {
         dap_enc_key_type_t l_pkey_exchange_type =DAP_ENC_KEY_TYPE_MSRLN ;
