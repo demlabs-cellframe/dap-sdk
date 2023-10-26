afb3e4723c4721ad5f1bfa4dcf63e54b20bb0e22
pavel.uhanov <pavel.uhanov@demlabs.net>
2023-10-26 23:36:31 +0700

    ...

--- crypto/src/sphincsplus/params.h
+++ crypto/src/sphincsplus/params.h
@@ -1,11 +1,7 @@
 #define str(s) #s
 #define xstr(s) str(s)
 
-#ifndef GLOBAL_PARAMS
-#define GLOBAL_PARAMS
 #include "sphincsplus_global.h"
-extern sphincsplus_params_t g_sphincsplus_params_current;
-#endif
 #ifndef SPHINCSPLUS_FLEX
 #include xstr(params/params-PARAMS.h)
 #endif
