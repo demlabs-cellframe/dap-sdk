7a74de07e196e115ba64c619c29d3ce7351aca33
alexey.stratulat <alexey.stratulat@demlabs.net>
2023-11-22 16:47:39 +0700

    [+] Added hash table for storing command aliases. Added functions for adding aliases and searching for an alias. Also, search by hash of alias tables has been added to the command processor.

--- net/server/cli_server/include/dap_cli_server.h
+++ net/server/cli_server/include/dap_cli_server.h
@@ -53,6 +53,13 @@ typedef struct dap_cli_cmd{
     UT_hash_handle hh;
 } dap_cli_cmd_t;
 
+typedef struct dap_cli_cmd_aliases{
+    char alias[32];
+    char addition[32];
+    dap_cli_cmd_t *standard_command;
+    UT_hash_handle hh;
+}dap_cli_cmd_aliases_t;
+
 
 int dap_cli_server_init(bool a_debug_more,const char * a_socket_path_or_address, uint16_t a_port, const char * a_permissions) ;
 void dap_cli_server_deinit();
@@ -65,3 +72,6 @@ void dap_cli_server_cmd_apply_overrides(const char * a_name, const dap_cli_serve
 
 dap_cli_cmd_t* dap_cli_server_cmd_get_first();
 dap_cli_cmd_t* dap_cli_server_cmd_find(const char *a_name);
+
+void dap_cli_server_alias_add(const char *a_alias, const char *a_pre_cmd, dap_cli_cmd_t *a_cmd);
+dap_cli_cmd_t *dap_cli_server_cmd_find_by_alias(const char *a_cli, char **a_append, char **a_ncmd);
