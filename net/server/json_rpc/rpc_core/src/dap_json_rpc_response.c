136214150f4ea21b405c1ec6d96d203ed5c62518
Constantin P <papizh.konstantin@demlabs.net>
2024-01-22 19:01:42 +0700

    ...

--- net/server/json_rpc/rpc_core/src/dap_json_rpc_response.c
+++ net/server/json_rpc/rpc_core/src/dap_json_rpc_response.c
@@ -86,7 +86,6 @@ void dap_json_rpc_response_free(dap_json_rpc_response_t *response)
             case TYPE_RESPONSE_ERROR:
                 if (response->json_arr_errors)
                     json_object_put(response->json_arr_errors);
-                // No specific cleanup needed for these response types
                 break;
             default:
                 log_it(L_ERROR, "Unsupported response type");
@@ -133,7 +132,7 @@ char* dap_json_rpc_response_to_string(const dap_json_rpc_response_t* response) {
 
     // json errors
     if (response->type == TYPE_RESPONSE_ERROR) {
-        json_object_object_add(jobj, "errors", response->json_arr_errors);
+        json_object_object_add(jobj, "errors", json_object_get(response->json_arr_errors));
     } else {
         json_object_object_add(jobj, "errors", json_object_new_null());
     }
