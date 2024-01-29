de5796f2df1ea633178faf9bb20502fda7f8af09
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-01-29 09:09:58 +0000

    Port 7672

--- net/server/cli_server/dap_cli_server.c
+++ net/server/cli_server/dap_cli_server.c
@@ -706,7 +706,8 @@ int json_commands(const char * a_name) {
     const char* long_cmd[] = {
             "tx_history",
             "mempool",
-            "chain_ca_copy"
+            "chain_ca_copy",
+            "net"
     };
     for (size_t i = 0; i < sizeof(long_cmd)/sizeof(long_cmd[0]); i++) {
         if (!strcmp(a_name, long_cmd[i])) {
