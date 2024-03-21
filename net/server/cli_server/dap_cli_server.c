4f6ddd53a3bbe3ec088f20d9b55189bae8741192
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-03-21 18:45:12 +0700

    [*] Changed the password change mechanism. Rewritten using the strstr function.

--- net/server/cli_server/dap_cli_server.c
+++ net/server/cli_server/dap_cli_server.c
@@ -785,32 +785,12 @@ char    *str_header;
                 else {
                     char *l_str_cmd = dap_strdup(str_cmd);
                     bool l_is_password = false;
-                    size_t l_size_start = 0;
-                    size_t l_size_end = 0;
-                    for (size_t i = 0; i < dap_strlen(l_str_cmd); i++) {
-                        if (l_str_cmd[i] == ';') {
-                            if (l_size_start == l_size_end) {
-                                l_size_end = i;
-                                if (l_is_password)
-                                    break;
-                            } else {
-                                l_size_start = l_size_end;
-                                l_size_end = i;
-                                if (l_is_password)
-                                    break;
-                            }
-                            if (l_size_end - l_size_start == 10 &&
-                                dap_strcmp(l_str_cmd + l_size_start, "-password")) {
-                                l_is_password = true;
-                                l_size_start = l_size_end;
-                            }
-                        }
-                    }
-                    if (l_size_start == l_size_end && l_size_end < dap_strlen(l_str_cmd))
-                        l_size_end = dap_strlen(l_str_cmd);
-                    if (l_is_password) {
-                        for (size_t i = l_size_start; i < l_size_end; i++) {
-                            l_str_cmd[i] = '*';
+                    char *l_ptr = strstr(l_str_cmd, "-password");
+                    if (l_ptr) {
+                        l_ptr += 10;
+                        while(l_ptr[0] != '\0' && l_ptr[0] != ';') {
+                            *l_ptr = '*';
+                            l_ptr +=1;
                         }
                     }
                     log_it(L_DEBUG, "execute command=%s", l_str_cmd);
