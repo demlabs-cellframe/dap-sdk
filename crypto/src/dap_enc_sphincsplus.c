23c6522ba03b1ea21aa4ba59d7bde3486c324d11
Pavel Uhanov <pavel.uhanov@demlabs.net>
2024-07-11 10:31:18 +0700

    [*] change sha to shake

--- crypto/src/dap_enc_sphincsplus.c
+++ crypto/src/dap_enc_sphincsplus.c
@@ -9,7 +9,7 @@
 static const sphincsplus_config_t s_default_config = SPHINCSPLUS_SHA2_128F;
 static const sphincsplus_difficulty_t s_default_difficulty = SPHINCSPLUS_SIMPLE;
 #else
-static _Thread_local sphincsplus_config_t s_default_config = SPHINCSPLUS_SHA2_128F;
+static _Thread_local sphincsplus_config_t s_default_config = SPHINCSPLUS_SHAKE_128F;
 static _Thread_local sphincsplus_difficulty_t s_default_difficulty = SPHINCSPLUS_SIMPLE;
 #endif
 
