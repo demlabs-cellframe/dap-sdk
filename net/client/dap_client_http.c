6771664cc645a4f613c45b94da58538dfb35715a
Olzhas <oljas.jarasbaev@demlabs.net>
2024-08-02 14:56:36 +0700

    [*] fix retcode

--- net/client/dap_client_http.c
+++ net/client/dap_client_http.c
@@ -68,7 +68,7 @@ static WOLFSSL_CTX *s_ctx;
 #endif
 
 http_status_code_t s_extract_http_code(void *a_response, size_t a_response_size) {
-    char l_http_code_str[3] = {'\0'};
+    char l_http_code_str[4] = {'\0'};
     size_t l_first_space = 0;
     for (;l_first_space < a_response_size; l_first_space++) {
         if (((const char*)a_response)[l_first_space] == ' ')
@@ -79,6 +79,7 @@ http_status_code_t s_extract_http_code(void *a_response, size_t a_response_size)
     l_http_code_str[0] = ((const char*)a_response)[l_first_space+1];
     l_http_code_str[1] = ((const char*)a_response)[l_first_space+2];
     l_http_code_str[2] = ((const char*)a_response)[l_first_space+3];
+    l_http_code_str[3] = '\0';
     http_status_code_t l_http_code = strtoul(l_http_code_str, NULL, 10);
     return l_http_code;
 }
