f6622ae7a74ffc7f7ee61583507522a7bdc10a85
Olzhas <oljas.jarasbaev@demlabs.net>
2024-08-16 16:21:54 +0700

    [*] fix log

--- crypto/src/dap_enc.c
+++ crypto/src/dap_enc.c
@@ -53,6 +53,7 @@ int dap_enc_init()
     dap_cert_init();
     dap_crc64_init();
     s_debug_more = g_config ? dap_config_get_item_bool_default(g_config, "crypto", "debug_more", false) : false;
+    dap_enc_sig_dilithium_init(s_debug_more);
     dap_sign_init(DAP_SIGN_HASH_TYPE_SHA3);
     return 0;
 }
