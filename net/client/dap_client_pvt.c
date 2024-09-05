6a84140bada3f0471025a253ba01dc6f46691e28
Constantin P. <papizh.konstantin@demlabs.net>
2024-09-05 11:47:44 +0700

    ...

--- net/client/dap_client_pvt.c
+++ net/client/dap_client_pvt.c
@@ -144,11 +144,8 @@ static void s_client_internal_clean(dap_client_pvt_t *a_client_pvt)
     }
     if (a_client_pvt->stream_es) {
         dap_stream_delete_unsafe(a_client_pvt->stream);
-        a_client_pvt->stream = NULL;
-        a_client_pvt->stream_es->_inheritor = NULL;
-        a_client_pvt->stream_es = NULL;
+        a_client_pvt->stream = a_client_pvt->stream_es = a_client_pvt->stream_key = NULL;
         a_client_pvt->stream_id = 0;
-        a_client_pvt->stream_key = NULL;
     }
 
     DAP_DEL_Z(a_client_pvt->session_key_id);
