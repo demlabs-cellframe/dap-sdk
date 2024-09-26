e108c43a71f59f5faca2e7cc10bbcbdef2745a91
alexey.stratulat <alexey.stratulat@demlabs.net>
2024-09-26 12:28:01 +0700

    [+] Added command find in list supported json_commands.

--- net/server/cli_server/dap_cli_server.c
+++ net/server/cli_server/dap_cli_server.c
@@ -190,7 +190,8 @@ int json_commands(const char * a_name) {
             "token",
             "esbocs",
             "global_db",
-            "net"
+            "net",
+            "find"
     };
     for (size_t i = 0; i < sizeof(long_cmd)/sizeof(long_cmd[0]); i++) {
         if (!strcmp(a_name, long_cmd[i])) {
