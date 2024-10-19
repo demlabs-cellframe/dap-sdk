0f9e06275aac79883d77185c22820f08fc21811c
Constantin P. <papizh.konstantin@demlabs.net>
2024-10-19 20:32:19 +0700

    ...

--- io/dap_net.c
+++ io/dap_net.c
@@ -51,13 +51,21 @@ int dap_net_resolve_host(const char *a_host, const char *a_port, bool a_numeric_
     struct addrinfo *l_res = NULL, l_hints = { .ai_flags = l_ai_flags, .ai_family = AF_UNSPEC, .ai_socktype = SOCK_STREAM };
     if (a_family)
         *a_family = AF_UNSPEC;
-    if (( l_ret = getaddrinfo(a_host, a_port, &l_hints, &l_res) ) || !l_res )
-        return log_it(L_ERROR, "getaddrinfo() failed, error %d \"%s\"", l_ret, gai_strerror(l_ret) ), -2;
-    if (a_family)
-        *a_family = l_res->ai_family;
-    l_ret = l_res->ai_addrlen;
-    memcpy(a_addr_out, l_res->ai_addr, l_res->ai_addrlen);
-    freeaddrinfo(l_res);
+    if (( l_ret = getaddrinfo(a_host, a_port, &l_hints, &l_res) ) || !l_res ) {
+#ifdef DAP_OS_WINDOWS
+        _set_errno( WSAGetLastError() );
+        log_it( L_ERROR, "getaddrinfo() failed, error %d: \"%s\"", errno, dap_strerror(errno) );
+#else
+        log_it( L_ERROR, "getaddrinfo() failed, error %d: \"%s\"", l_ret, gai_strerror(l_ret) );
+#endif
+        l_ret = -2;
+    } else {
+        if (a_family)
+            *a_family = l_res->ai_family;
+        l_ret = l_res->ai_addrlen;
+        memcpy(a_addr_out, l_res->ai_addr, l_res->ai_addrlen);
+        freeaddrinfo(l_res);
+    }
     return l_ret;
 }
 
