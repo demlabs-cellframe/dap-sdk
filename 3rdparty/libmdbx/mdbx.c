ed57fa2298ef16f579aab6ac13fb45dea499c509
pavel.uhanov <pavel.uhanov@demlabs.net>
2024-11-19 06:30:55 +0300

    [*] test

--- 3rdparty/libmdbx/mdbx.c
+++ 3rdparty/libmdbx/mdbx.c
@@ -202,7 +202,7 @@
 #define SSIZE_MAX INTPTR_MAX
 #endif
 
-#if UINTPTR_MAX > 0xffffFFFFul || ULONG_MAX > 0xffffFFFFul || defined(_WIN64)
+#if UINTPTR_MAX > 0xffffFFFFul || ULONG_MAX > 0xffffFFFFul
 #define MDBX_WORDBITS 64
 #else
 #define MDBX_WORDBITS 32
