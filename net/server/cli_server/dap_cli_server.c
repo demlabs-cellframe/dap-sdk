f399a06c828eeab1b6bea04427a6922c0bdcbc10
roman.padenkov <roman.padenkov@demlabs.net>
2025-01-22 18:25:31 +0700

    ...

--- net/server/cli_server/dap_cli_server.c
+++ net/server/cli_server/dap_cli_server.c
@@ -211,6 +211,7 @@ int json_commands(const char * a_name) {
             "srv_stake",
             "voting",
             "emit_delegate",
+            "srv_datum",
             "exec_cmd"
     };
     for (size_t i = 0; i < sizeof(long_cmd)/sizeof(long_cmd[0]); i++) {
