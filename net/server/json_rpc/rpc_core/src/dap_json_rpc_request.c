1ffe5125ea88a2b36eb9cd865c23d31a3392cc2a
daniil.frolov <daniil.frolov@demlabs.net>
2025-01-23 16:01:38 +0300

    ..

--- net/server/json_rpc/rpc_core/src/dap_json_rpc_request.c
+++ net/server/json_rpc/rpc_core/src/dap_json_rpc_request.c
@@ -237,6 +237,7 @@ dap_json_rpc_request_t *dap_json_rpc_request_from_json(const char *a_data)
                 *jobj_id = NULL,
                 *jobj_method = NULL,
                 *jobj_params = NULL,
+                *jobj_subcmd = NULL,
                 *l_arguments_obj = NULL;
     if (jterr == json_tokener_success)
         do {
@@ -254,10 +255,14 @@ dap_json_rpc_request_t *dap_json_rpc_request_from_json(const char *a_data)
                 break;
             }
 
-            if (json_object_object_get_ex(jobj, "params", &jobj_params))
+            json_object_object_get_ex(jobj, "params", &jobj_params);
+            json_object_object_get_ex(jobj, "subcommand", &jobj_subcmd);
+            json_object_object_get_ex(jobj, "arguments", &l_arguments_obj);
+
+            if (jobj_params)
                 request->params = dap_json_rpc_params_create_from_array_list(jobj_params);
-            else if(json_object_object_get_ex(jobj, "arguments", &l_arguments_obj) || json_object_object_get_ex(jobj, "subcommand", &jobj_params)){
-                    request->params = dap_json_rpc_params_create_from_subcmd_and_args(jobj_params, l_arguments_obj, request->method);
+            else if(jobj_subcmd || l_arguments_obj){
+                request->params = dap_json_rpc_params_create_from_subcmd_and_args(jobj_subcmd, l_arguments_obj, request->method);
             } else {
                 log_it(L_ERROR, "Error parse JSON string, Can't find array params or subcomand and arguments for request with id: %" DAP_UINT64_FORMAT_U, request->id);
                 break;
