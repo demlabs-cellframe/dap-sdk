4b0fd7822ec9d86d91325dc10ebc2cd1cd5743e9
pavel.uhanov <pavel.uhanov@demlabs.net>
2025-03-17 07:28:38 +0000

    backport-15029

--- net/server/cli_server/dap_cli_server.c
+++ net/server/cli_server/dap_cli_server.c
@@ -262,7 +262,8 @@ int json_commands(const char * a_name) {
             "stats",
             "print_log",
             "stake_lock",
-            "exec_cmd"            
+            "exec_cmd",
+            "policy"
     };
     for (size_t i = 0; i < sizeof(long_cmd)/sizeof(long_cmd[0]); i++) {
         if (!strcmp(a_name, long_cmd[i])) {
