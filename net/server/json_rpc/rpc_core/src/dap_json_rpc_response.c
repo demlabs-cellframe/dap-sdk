7eed9a1aa58f880e97658ac24cd3f848f498f40e
roman.padenkov <roman.padenkov@demlabs.net>
2025-05-22 19:46:54 +0700

    ...

--- net/server/json_rpc/rpc_core/src/dap_json_rpc_response.c
+++ net/server/json_rpc/rpc_core/src/dap_json_rpc_response.c
@@ -310,11 +310,9 @@ int dap_json_rpc_response_printf_result(dap_json_rpc_response_t* response, char 
                 default: {
                         
                         dap_cli_cmd_t *l_cmd = dap_cli_server_cmd_find(cmd_name);
-                        if (l_cmd){
-                            l_cmd->func_rpc(response, cmd_params, cmd_cnt);
-                        }
-                        else
+                        if (!l_cmd || l_cmd->func_rpc(response, cmd_params, cmd_cnt)){
                             json_print_object(response->result_json_object, 0);
+                        }
                     }
                 break;
             }
