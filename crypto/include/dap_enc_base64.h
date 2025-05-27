536a2d0b27469593238869e63425f7d78c2a2d29
Roman Khlopkov <roman.khlopkov@demlabs.net>
2025-05-27 08:16:26 +0000

    bugfix-17365

--- crypto/include/dap_enc_base64.h
+++ crypto/include/dap_enc_base64.h
@@ -35,7 +35,7 @@ extern "C" {
  * Calculates encode size from input size
  */
 #define DAP_ENC_BASE64_ENCODE_SIZE(in_size) (size_t)(((4 * (in_size) / 3) + 3) & ~3)
-#define DAP_ENC_BASE64_DECODE_SIZE(in_size) (size_t)((in_size) * 3 / 4)
+#define DAP_ENC_BASE64_DECODE_SIZE(in_size) (size_t)((in_size) * 3 / 4 + (in_size) % 4)
 
 size_t dap_enc_base64_decode(const char * in, size_t in_size, void * out, dap_enc_data_type_t standard);
 size_t dap_enc_base64_encode(const void * in, size_t in_size, char * out, dap_enc_data_type_t standard);
