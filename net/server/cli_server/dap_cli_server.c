40bcf0e846ea26a680a32b28d1b5e226223e19ae
pavel.uhanov <pavel.uhanov@demlabs.net>
2025-07-31 18:04:32 +0300

    [*] tests

--- net/server/cli_server/dap_cli_server.c
+++ net/server/cli_server/dap_cli_server.c
@@ -271,7 +271,8 @@ int json_commands(const char * a_name) {
             "print_log",
             "stake_lock",
             "exec_cmd",
-            "policy"
+            "policy",
+            "composer"
     };
     for (size_t i = 0; i < sizeof(long_cmd)/sizeof(long_cmd[0]); i++) {
         if (!strcmp(a_name, long_cmd[i])) {
