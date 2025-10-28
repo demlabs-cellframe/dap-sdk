866a104bac267a338f1e0e09950b32f6672bb6f3
Dmitriy Gerasimov <ceo@demlabs.net>
2025-10-28 18:01:21 +0700

    feat(tests): Reorganize test infrastructure and add mock autowrap to all tests
    - Move tests from net/*/test to tests/{unit,integration}/net/* structure
    - Add dap_mock_autowrap() to all test CMakeLists.txt files:
      - core/test/CMakeLists.txt
      - io/test/CMakeLists.txt
      - global-db/test/CMakeLists.txt
      - tests/integration/net/server/CMakeLists.txt
      - tests/integration/net/stream/CMakeLists.txt
    - Remove dap_test_mocks library dependency (replaced by dap_test)
    - Comment out old test subdirectories in net/client/CMakeLists.txt
    - Delete obsolete test directories: net/client/test, net/server/test, net/stream/test
    - Add auto-load section to vpn_test_coverage.json task with all required modules
    - Update autoload_system documentation in manifest.json
    - All tests now use unified DAP Mock Framework with automatic wrapper generation

--- net/stream/test/dap_stream_test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "dap_common.h"
-
-void dap_stream_test_init(void);
-void dap_stream_test_run(const char *a_ip_addr_str, uint16_t a_port, size_t a_data_size, int a_pkt_count);
-
-// Static function prototype
-static int s_cli_stream_test(int argc, char **argv, char **a_str_reply, int a_version);
