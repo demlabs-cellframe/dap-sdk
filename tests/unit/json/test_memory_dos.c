721fe73a58f349d2710352fb7c9e83df52c6b221
Dmitry Pyzyrkov <dpuzyrkov@gmail.com>
2026-02-24 17:05:45 +0700

    ...

--- tests/unit/json/test_memory_dos.c
+++ tests/unit/json/test_memory_dos.c
@@ -371,7 +371,7 @@ static bool s_test_algorithmic_complexity(void) {
 
     // Larger sizes to get stable timing (milliseconds, not microseconds)
     const int sizes[] = {2000, 4000, 8000};
-    const int NUM_RUNS = 5;
+#define NUM_RUNS 5
     double times[3];
 
     // Warmup: parse objects of similar size to stabilize CPU caches,
