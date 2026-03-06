50965e92d6fef6bf3e66fa653bc9ec5a428160f6
roman.padenkov <roman.padenkov@demlabs.net>
2026-03-06 15:27:35 +0700

    ...

--- module/core/include/dap_common.h
+++ module/core/include/dap_common.h
@@ -74,7 +74,7 @@
 #include "dap_bit_ops.h"
 
 #define BIT( x ) ( 1 << x )
-#define __has_builtin(x) (0)
+
 // Stuffs an integer into a pointer type
 #define DAP_INT_TO_POINTER(i) ((void*) (size_t) (i))
 // Extracts an integer from a pointer
